74327e81e553dcbc724880f79f2f0c129d2d3fd5
motodev222 <motodev100app@gmail.com>
2025-04-12 23:23:46 +0900

    更新

--- godot_mystery.service.worker.js
+++ godot_mystery.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744431866|62007753068';
+const CACHE_VERSION = '1744467486|97627774306';
 /** @type {string} */
 const CACHE_PREFIX = 'godot_mystery-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
